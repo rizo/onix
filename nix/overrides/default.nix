89656772cd947dce8c5f526ba7dc1bec5fac252a
Rizo I <rizo@odis.io>
2022-05-22 13:24:13 +0100

    Add a patch for core_unix.

--- nix/overrides/default.nix
+++ nix/overrides/default.nix
@@ -5,4 +5,10 @@
       patches = [ ./ocamlfind/ldconf.patch ./ocamlfind/install_topfind.patch ];
     });
 
+    core_unix = pkg: pkg.overrideAttrs (super: {
+      prePatch =
+        super.prePatch + ''
+          patchShebangs unix_pseudo_terminal/src/discover.sh
+        '';
+    });
 }
