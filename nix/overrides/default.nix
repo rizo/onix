adb6eaa4a7e56f683814eeb9c154ad9c4894904f
Rizo I <rizo@odis.io>
2022-05-23 17:47:44 +0100

    Add override for conf-binutils.

--- nix/overrides/default.nix
+++ nix/overrides/default.nix
@@ -5,10 +5,17 @@
       patches = [ ./ocamlfind/ldconf.patch ./ocamlfind/install_topfind.patch ];
     });
 
-    core_unix = pkg: pkg.overrideAttrs (super: {
-      prePatch =
-        super.prePatch + ''
-          patchShebangs unix_pseudo_terminal/src/discover.sh
-        '';
+  conf-binutils = pkg:
+    pkg.overrideAttrs (super: {
+      OBJDUMP_PATH = "${pkgs.binutils-unwrapped}/bin/objdump";
+      CXXFILT_PATH = "${pkgs.binutils-unwrapped}/bin/c++filt";
+      READELF_PATH = "${pkgs.binutils-unwrapped}/bin/readelf";
+    });
+
+  core_unix = pkg:
+    pkg.overrideAttrs (super: {
+      prePatch = super.prePatch + ''
+        patchShebangs unix_pseudo_terminal/src/discover.sh
+      '';
     });
 }
