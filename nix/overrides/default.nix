2b7446b2fc011aa6cbf2ef3d2221fd7172062306
Rizo I <rizo@odis.io>
2022-05-26 21:32:18 +0100

    Add darwin overrides.

--- nix/overrides/default.nix
+++ nix/overrides/default.nix
@@ -1,20 +1,35 @@
-{ pkgs, ocaml, scope }: {
+{ pkgs, ocaml, scope }:
 
-  ocamlfind = pkg:
-    pkg.overrideAttrs (super: {
-      patches = [ ./ocamlfind/ldconf.patch ./ocamlfind/install_topfind.patch ];
-    });
+let
+  common = {
+    ocamlfind = pkg:
+      pkg.overrideAttrs (super: {
+        patches =
+          [ ./ocamlfind/ldconf.patch ./ocamlfind/install_topfind.patch ];
+      });
 
-  # https://github.com/ocsigen/lwt/pull/946
-  lwt_react = pkg:
-    pkg.overrideAttrs (super: {
-      nativeBuildInputs = super.nativeBuildInputs ++ [ scope.cppo or null ];
-    });
+    # https://github.com/ocsigen/lwt/pull/946
+    lwt_react = pkg:
+      pkg.overrideAttrs (super: {
+        nativeBuildInputs = super.nativeBuildInputs ++ [ scope.cppo or null ];
+      });
 
-  core_unix = pkg:
-    pkg.overrideAttrs (super: {
-      prePatch = super.prePatch + ''
-        patchShebangs unix_pseudo_terminal/src/discover.sh
-      '';
-    });
-}
+    core_unix = pkg:
+      pkg.overrideAttrs (super: {
+        prePatch = super.prePatch + ''
+          patchShebangs unix_pseudo_terminal/src/discover.sh
+        '';
+      });
+  };
+
+  darwin = {
+    dune = pkg:
+      pkg.overrideAttrs (super: {
+        buildInputs = super.buildInputs or [ ] ++ [
+          pkgs.darwin.apple_sdk.frameworks.Foundation
+          pkgs.darwin.apple_sdk.frameworks.CoreServices
+        ];
+      });
+  };
+
+in common // pkgs.lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin darwin
