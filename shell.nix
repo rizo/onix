31f5b6a04f8ed72f15dcf391991848b3999a8573
Rizo I <rizo@odis.io>
2022-05-29 14:51:03 +0100

    Add dev tools.

--- shell.nix
+++ shell.nix
@@ -1,3 +1,6 @@
 { pkgs ? import <nixpkgs> { } }:
 let onix = import ./. { inherit pkgs; };
-in pkgs.mkShell { inputsFrom = [ onix ]; }
+in pkgs.mkShell {
+  inputsFrom = [ onix ];
+  buildInputs = [ pkgs.nixfmt pkgs.ocamlPackages.ocaml-lsp pkgs.ocamlformat ];
+}
